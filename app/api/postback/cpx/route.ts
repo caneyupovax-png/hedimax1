59bf87e1880b50ec0a4e788a974e867bf6932789
can <mailin@ornek.com>
2025-12-28 21:13:04 +0300

    update

--- app/api/postback/cpx/route.ts
+++ app/api/postback/cpx/route.ts
@@ -1,9 +1,13 @@
+import { NextRequest } from "next/server";
+
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  // her durumda 200 OK
   return new Response("OK", { status: 200 });
 }
-export async function POST() {
+
+export async function POST(req: NextRequest) {
   return new Response("OK", { status: 200 });
 }
